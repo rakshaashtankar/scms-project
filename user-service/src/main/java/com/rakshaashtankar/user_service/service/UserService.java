9dfb9fb6fd84d6a5697722c7dfc24fc09b26c8aa
Raksha Ashtankar <rakshaashtankar@Rakshas-MacBook-Air.local>
2025-09-07 23:57:44 +0530

    feat(service): update UserService

--- user-service/src/main/java/com/rakshaashtankar/user_service/service/UserService.java
+++ user-service/src/main/java/com/rakshaashtankar/user_service/service/UserService.java
@@ -1,5 +1,8 @@
 package com.rakshaashtankar.user_service.service;
 
+import com.rakshaashtankar.user_service.dto.PasswordChangeRequest;
+import com.rakshaashtankar.user_service.dto.UserCreateRequest;
+import com.rakshaashtankar.user_service.dto.UserPatchRequest;
 import com.rakshaashtankar.user_service.model.User;
 
 import java.util.List;
@@ -8,9 +11,9 @@ import java.util.Optional;
 public interface UserService {
     List<User> getAllUsers();
     Optional<User> getUserById(Long id);
-    User createUser(User user);
+    User createUser(UserCreateRequest request);
     User updateUser(Long id, User user);
+    User patchUser(Long id, UserPatchRequest patchRequest);
     boolean deleteUser(Long id);
-
-
+    void changePassword(Long id, PasswordChangeRequest request);
 }
