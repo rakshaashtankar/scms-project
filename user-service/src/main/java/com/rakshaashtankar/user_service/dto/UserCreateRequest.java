f5b2dcd2332270d51ccc70b66ba7c3207f336162
Raksha Ashtankar <rakshaashtankar@Rakshas-MacBook-Air.local>
2025-09-07 16:00:10 +0530

    feat(dto): add UserCreateRequest file

--- user-service/src/main/java/com/rakshaashtankar/user_service/dto/UserCreateRequest.java
+++ user-service/src/main/java/com/rakshaashtankar/user_service/dto/UserCreateRequest.java
@@ -1,4 +1,34 @@
+// DTO for creating a user (Admin registers users)
 package com.rakshaashtankar.user_service.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserCreateRequest {
+    @NotBlank(message = "Username is required")
+    @Size(min = 5, max = 50)
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min=8, message = "Password must be at least 8 characters")
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$",
+            message = "Password must be at least 8 characters long, contain digit, uppercase, lowercase and special character, and have no whitespace"
+    )
+    private String password;
+
+    @NotBlank(message = "Role is required")
+    private String role;
 }
