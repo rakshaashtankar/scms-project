b2765add059d9159e44fa50ba472ba451516a356
Raksha Ashtankar <rakshaashtankar@Rakshas-MacBook-Air.local>
2025-09-07 23:02:43 +0530

    feat: add PasswordChangeRequest

---
+++ user-service/src/main/java/com/rakshaashtankar/user_service/dto/PasswordChangeRequest.java
@@ -0,0 +1,25 @@
+package com.rakshaashtankar.user_service.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PasswordChangeRequest {
+
+    @NotBlank
+    private String oldPassword;
+
+    @NotBlank
+    @Size(min=8, message = "Password must be at least 8 characters")
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$",
+            message = "Password must be at least 8 characters long, contain digit, uppercase, lowercase and special character, and have no whitespace"
+    )
+    private String newPassword;
+}
