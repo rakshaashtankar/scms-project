25fdc7147e39c826c055dd3f9f0c4fe507f5a5ad
Raksha Ashtankar <rakshaashtankar@Rakshas-MacBook-Air.local>
2025-09-07 15:37:22 +0530

    feat: add passwordChanged field to User entity

---
+++ user-service/src/main/java/com/rakshaashtankar/user_service/dto/UserUpdateRequest.java
@@ -0,0 +1,4 @@
+package com.rakshaashtankar.user_service.dto;
+
+public class UserUpdateRequest {
+}
--- user-service/src/main/java/com/rakshaashtankar/user_service/model/User.java
+++ user-service/src/main/java/com/rakshaashtankar/user_service/model/User.java
@@ -5,37 +5,33 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "Username is required")
-    @Size(min = 5, max = 50)
     @Column(nullable = false, unique = true, length = 50)
     private String username;
 
-    @NotBlank(message = "Email is required")
-    @Email(message = "Invalid email format")
     @Column(nullable = false, unique = true, length = 100)
     private String email;
 
-    @NotBlank(message = "Password is required")
-    @Size(min=8, message = "Password must be at least 6 characters")
-    @Pattern(
-            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$",
-            message = "Password must be at least 8 characters long, contain digit, uppercase, lowercase and special character, and have no whitespace"
-    )
     @Column(nullable = false)
     private String password;
 
-    @NotBlank(message = "Role is required")
     @Column(nullable = false)
     private String role;
+
+    @Column(nullable = false)
+    private boolean passwordChanged = false;
 }
